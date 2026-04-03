851d439e1413113ee926bfc139cc9cb9f60049e9
RiwajSt11 <shresthariwaj178@gmail.com>
2026-04-03 08:06:23 +0545

    next config basepath attached

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/resource-sharing-frontend",
+  assetPrefix: "/resource-sharing-frontend/",
 };
 
 export default nextConfig;
